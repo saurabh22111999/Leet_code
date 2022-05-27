7f458786940aa86d871896f53a681a327bcf2a19
Saurabh jangra <55055472+saurabh22111999@users.noreply.github.com>
2022-05-27 08:52:52 +0530

    Time: 3 ms (54.81%), Space: 6 MB (48.75%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -7,14 +7,14 @@ public:
         int num = n;
         int count = 0;
         unsigned long res1 = static_cast<unsigned long>(num);
-unsigned long res2 = static_cast<unsigned long>(count);
+
 
         while(res1 != 0){
             int mask = res1 & (-res1);
             res1 = res1 - mask;
-            res2++;
+            count++;
         }
-        return res2;
+        return count;
     }
         
     
